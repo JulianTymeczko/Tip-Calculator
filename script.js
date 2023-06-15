680e346039f4a58bb640800c08c182b238eef14c
Julian Houle-T <jhouletymeczko@gmail.com>
2023-06-15 03:37:37 -0400

    added specific color scheme to resemble the mock-up

---
+++ script.js
@@ -0,0 +1,8 @@
+var tipResult = document.getElementById("Tip-Result")
+var totalResult = document.getElementById("Total-Result")
+
+  tipResult.textContent 
+
+
+
+  $(".btn-secondary").addClass("shadow-none");
