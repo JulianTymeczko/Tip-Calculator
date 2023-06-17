d162225f634e873a0e6f91da3af6b8cd22443d38
Julian Houle-T <jhouletymeczko@gmail.com>
2023-06-17 02:00:58 -0400

    finished more javascript aspects

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ var fixedTipPercentage = document.getElementsByClassName("fixed-percentage")
 var customTipPercentage = document.getElementById("custom-percentage")
 var x = ""
 var y = ""
-
+var resetButton = document.getElementById("reset-Button")
 for (var i = 0; i < fixedTipPercentage.length; i++) {
     fixedTipPercentage[i].addEventListener("click", function () {
         x = this.textContent
-        var tip = ""
+        
         if (bill.value != "" && numberOfPeople.value != "") {
             y = ((parseInt(bill.value) * (parseInt(x) / 100) / parseInt(numberOfPeople.value)).toString()).split(".")
             if (y[1] != undefined && y[1].length >= 2) {
@@ -37,26 +37,59 @@ for (var i = 0; i < fixedTipPercentage.length; i++) {
 
 }
 
-while (bill.value != "" && numberOfPeople.value != "" && customTipPercentage.value != ""){
-    y = ((parseInt(bill.value) * (parseInt(customTipPercentage.value) / 100) / parseInt(numberOfPeople.value)).toString()).split(".")
-    if (y[1] != undefined && y[1].length >= 2) {
+bill.addEventListener("input", function () {
+    
+    
+    if ((x != "" || customTipPercentage.value != "") && numberOfPeople.value != "" && bill.value != ""  ) {
+        y = ((parseInt(bill.value) * (parseInt(x) / 100) / parseInt(numberOfPeople.value)).toString()).split(".")
+        if (y[1] != undefined && y[1].length >= 2) {
 
-        tipResult.textContent = `$${y[0]}.${y[1].slice(0, 2)}`
-        totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+            tipResult.textContent = `$${y[0]}.${y[1].slice(0, 2)}`
+            totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+        }
+        else if (y[1] != undefined && y[1].length == 1) {
+
+            tipResult.textContent = `$${y[0]}.${y[1]}0`
+            totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+        }
+        else if (y[1] == undefined) {
+
+            tipResult.textContent = `$${y[0]}.00`
+            totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+        }
+        
     }
-    else if (y[1] != undefined && y[1].length == 1) {
 
-        tipResult.textContent = `$${y[0]}.${y[1]}0`
-        totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+})
+
+numberOfPeople.addEventListener("input", function () {
+    
+    
+    if ((x != "" || customTipPercentage.value != "") && bill.value != "" && numberOfPeople.value != "") {
+        y = ((parseInt(bill.value) * (parseInt(x) / 100) / parseInt(numberOfPeople.value)).toString()).split(".")
+        if (y[1] != undefined && y[1].length >= 2) {
+
+            tipResult.textContent = `$${y[0]}.${y[1].slice(0, 2)}`
+            totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+        }
+        else if (y[1] != undefined && y[1].length == 1) {
+
+            tipResult.textContent = `$${y[0]}.${y[1]}0`
+            totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+        }
+        else if (y[1] == undefined) {
+
+            tipResult.textContent = `$${y[0]}.00`
+            totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+        }
+        
     }
-    else if (y[1] == undefined) {
 
-        tipResult.textContent = `$${y[0]}.00`
-        totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
-    }
-}
+})
 
-
+resetButton.addEventListener("click", function () {
+    location.reload()
+})
 
 
 
