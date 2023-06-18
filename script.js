3d8d893a661bf86b99507c1c43564460472f879a
Julian Houle-T <jhouletymeczko@gmail.com>
2023-06-18 02:09:46 -0400

    solved problem with negative inputs and javacsript logic

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ var resetButton = document.getElementById("reset-Button")
 for (var i = 0; i < fixedTipPercentage.length; i++) {
     fixedTipPercentage[i].addEventListener("click", function () {
         x = this.textContent
-        
-        if (bill.value != "" && numberOfPeople.value != "") {
+
+        if (bill.value != "" && numberOfPeople.value != "" && parseInt(numberOfPeople.value) > 0 && parseInt(bill.value) > 0) {
             y = ((parseInt(bill.value) * (parseInt(x) / 100) / parseInt(numberOfPeople.value)).toString()).split(".")
             if (y[1] != undefined && y[1].length >= 2) {
 
@@ -30,7 +30,7 @@ for (var i = 0; i < fixedTipPercentage.length; i++) {
                 tipResult.textContent = `$${y[0]}.00`
                 totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
             }
-            
+
         }
 
     })
@@ -38,9 +38,9 @@ for (var i = 0; i < fixedTipPercentage.length; i++) {
 }
 
 bill.addEventListener("input", function () {
-    
-    
-    if ((x != "" || customTipPercentage.value != "") && numberOfPeople.value != "" && bill.value != ""  ) {
+
+
+    if ((x != "" || customTipPercentage.value != "") && numberOfPeople.value != "" && bill.value != "" && parseInt(numberOfPeople.value) > 0 && parseInt(bill.value) > 0) {
         y = ((parseInt(bill.value) * (parseInt(x) / 100) / parseInt(numberOfPeople.value)).toString()).split(".")
         if (y[1] != undefined && y[1].length >= 2) {
 
@@ -57,15 +57,15 @@ bill.addEventListener("input", function () {
             tipResult.textContent = `$${y[0]}.00`
             totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
         }
-        
+
     }
 
 })
 
 numberOfPeople.addEventListener("input", function () {
-    
-    
-    if ((x != "" || customTipPercentage.value != "") && bill.value != "" && numberOfPeople.value != "") {
+
+
+    if ((x != "" || customTipPercentage.value != "") && bill.value != "" && numberOfPeople.value != "" && parseInt(numberOfPeople.value) > 0 && parseInt(bill.value) > 0) {
         y = ((parseInt(bill.value) * (parseInt(x) / 100) / parseInt(numberOfPeople.value)).toString()).split(".")
         if (y[1] != undefined && y[1].length >= 2) {
 
@@ -82,37 +82,38 @@ numberOfPeople.addEventListener("input", function () {
             tipResult.textContent = `$${y[0]}.00`
             totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
         }
-        
+
     }
 
 })
 customTipPercentage.addEventListener("input", function () {
-    
-    if ((customTipPercentage.value).includes(".")){
+
+    if ((customTipPercentage.value).includes(".")) {
         return;
     }
-   else if (!((customTipPercentage.value).includes("."))){
-    if (customTipPercentage.value != "" && bill.value != "" && numberOfPeople.value != "") {
-        y = ((parseInt(bill.value) * (parseInt(customTipPercentage.value) / 100) / parseInt(numberOfPeople.value)).toString()).split(".")
-        if (y[1] != undefined && y[1].length >= 2) {
+    else if (!((customTipPercentage.value).includes("."))) {
+        if (customTipPercentage.value != "" && bill.value != "" && numberOfPeople.value != "" && parseInt(numberOfPeople.value) > 0 && parseInt(bill.value) > 0 && parseInt(customTipPercentage.value) > 0) {
+            y = ((parseInt(bill.value) * (parseInt(customTipPercentage.value) / 100) / parseInt(numberOfPeople.value)).toString()).split(".")
+            if (y[1] != undefined && y[1].length >= 2) {
 
-            tipResult.textContent = `$${y[0]}.${y[1].slice(0, 2)}`
-            totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
-        }
-        else if (y[1] != undefined && y[1].length == 1) {
+                tipResult.textContent = `$${y[0]}.${y[1].slice(0, 2)}`
+                totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+            }
+            else if (y[1] != undefined && y[1].length == 1) {
 
-            tipResult.textContent = `$${y[0]}.${y[1]}0`
-            totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
-        }
-        else if (y[1] == undefined) {
+                tipResult.textContent = `$${y[0]}.${y[1]}0`
+                totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+            }
+            else if (y[1] == undefined) {
 
-            tipResult.textContent = `$${y[0]}.00`
-            totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+                tipResult.textContent = `$${y[0]}.00`
+                totalResult.textContent = "$" + (parseFloat(tipResult.textContent.slice(1)) + parseInt(bill.value) / parseInt(numberOfPeople.value)).toFixed(2)
+            }
+
         }
-        
+
     }
-
-}})
+})
 resetButton.addEventListener("click", function () {
     location.reload()
 })
